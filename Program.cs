1176cd934d4115465fe5fb452c32aee121630356
SamuelVimes7 <100790985+SamuelVimes7@users.noreply.github.com>
2023-09-27 06:21:30 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {                      
+            Console.WriteLine("Ivskite valiutos koda");
+            string tp, ccy, dtFrom, dtTo;
+            tp = "LT";
+            try
+            {
+                ccy = Console.ReadLine();
+                dtFrom = DateTime.Now.ToString("yyyy-MM-dd");
+                dtTo = DateTime.Now.ToString("yyyy-MM-dd");
+                FxRatesService.FxRatesSoapClient client = new FxRatesService.FxRatesSoapClient();
+                var result = client.getFxRatesForCurrency(tp, ccy, dtFrom, dtTo).InnerText;
+                Console.WriteLine(result.Remove(0, 19));
+                Console.ReadLine();
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine("Tokio valiutos kodo nera");
+                Console.ReadLine();
+            }
+        }
+    }
+}
